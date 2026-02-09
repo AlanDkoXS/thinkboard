b70e30eb900b15909599935088a12b92716fbf2d
Alan Quintana <alan.quintanapacheco@gmail.com>
2026-02-09 13:39:07 -0600

    :rocket:DEPLOYING: Ready, here we go!

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,13 +9,17 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+if (process.eventNames.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
+
 app.use(express.json()); // This middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 // Our simple custom midleware
@@ -25,6 +29,16 @@ app.use(rateLimiter);
   });
   */
 app.use("/api/notes", notesRoutes);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, ",,/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
